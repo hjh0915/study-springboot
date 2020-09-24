9542080be7748466102d4c9a17e3f037fb7caeb4
hjh0915 <623299157@qq.com>
2020-09-24 11:45:32 +0800

    学习Thymeleaf

--- WebApplication/src/main/java/com/example/controller/MyController.java
+++ WebApplication/src/main/java/com/example/controller/MyController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MyController {
-    
+    @RequestMapping("/hellosucc")
+    public String helloSucc(Map<String,Object> maps) {
+        maps.put("msg", "thymeleaf使用方法");
+        return "base";
+    }
 }
